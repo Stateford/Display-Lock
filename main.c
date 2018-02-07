bc4b4739a8e142fc719c52683a5373a81e49382e
idietmoran <idietmoran@gmail.com>
2018-02-07 00:00:52 -0500

    cleaned up main

--- main.c
+++ main.c
@@ -1,17 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <memory.h>
-#include <string.h>
-#include <Windows.h>
-
-#include "src\win.h"
 #include "src\menu.h"
 
 int main()
 {
-
-    WINDOWLIST foo;
-
     
     start();
 
