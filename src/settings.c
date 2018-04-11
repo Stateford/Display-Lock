eaad88ae04a41aa9a52208785bd684e489459a4c
idietmoran <idietmoran@gmail.com>
2018-04-11 01:33:13 -0400

    added file i/o

--- src/settings.c
+++ src/settings.c
@@ -1,5 +1,9 @@
 #include "settings.h"
 #include <stdio.h>
+#include <ShlObj.h>
+#include <shtypes.h>
+#include "Shlwapi.h"
+
 
 // initalizes settings window on startup
 void initalizeSettings(HWND hDlg, SETTINGS* settings)
@@ -29,9 +33,24 @@ void updateSettings(HWND hDlg, SETTINGS* settings)
 // TODO: This should probably save to a temp folder instead of a local folder to make the program more portable
 void writeSettings(SETTINGS settings)
 {
-    FILE *file = fopen("settings.DLOCK", "w");
+    PWSTR path = NULL;
+    HRESULT hr = SHGetKnownFolderPath(&FOLDERID_RoamingAppData, 0, 0, &path);
+    /*
+    if(SUCCEEDED(hr))
+    {
+        
+    }
+    */
+    // TODO: cannot create folder if it doesn't exist
+    PathAppend(path, TEXT("DisplayLock\\settings.DLOCK"));
+    
+    FILE *file = _wfopen(path, TEXT("w"));
+ 
+    // TODO : Need to create directory if it does not exist
+
     fwrite(&settings, sizeof(settings), 1, file);
     fclose(file);
+    CoTaskMemFree(path);
 }
 
 void readSettings(SETTINGS *settings)
