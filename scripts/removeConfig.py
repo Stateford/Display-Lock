70af71d49417b5b0638939ccb426ef8c7eb6fe63
idietmoran <idietmoran@gmail.com>
2018-09-13 22:07:01 -0400

    added error handling

--- scripts/removeConfig.py
+++ scripts/removeConfig.py
@@ -5,7 +5,11 @@ def main():
     configPath = os.getenv("APPDATA")
     configPath += "\\displayLock"
     print (configPath)
-    shutil.rmtree(configPath)
+    try:
+        shutil.rmtree(configPath)
+        print("directory cleaned")
+    except:
+        print("directory doesn't exist")
 
 
 main()
