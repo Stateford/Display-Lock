a13dfa11a8d6065f56cec33d97edbe7079c8e811
idietmoran <idietmoran@gmail.com>
2018-09-13 20:30:44 -0400

    added read/write

--- src/settings.c
+++ src/settings.c
@@ -1,5 +1,8 @@
 #include "settings.h"
 #include <stdio.h>
+#include <ShlObj.h>
+#include <shtypes.h>
+#include "Shlwapi.h"
 
 void initalizeSettings(HWND hDlg, SETTINGS_VIEW_CONTROLS *settingsControls, SETTINGS *settings)
 {
@@ -58,10 +61,58 @@ BOOL checkVersion(SETTINGS *settings)
 
 BOOL readSettings(SETTINGS *settings)
 {
+    PWSTR path;
+
+    // TODO: check header of file to ensure it's a valid config file using strcmp
+
+    if (SUCCEEDED(SHGetKnownFolderPath(&FOLDERID_RoamingAppData, 0, NULL, &path)))
+    {
+        PathAppendW(path, TEXT("DisplayLock\\settings.DLOCK"));
+        FILE *file = _wfopen(path, TEXT("r"));
+
+        // if if opening file is succcessful read into struct
+        // otherwise use default settings
+        if (file != NULL && file == 0)
+        {
+            fread(settings, sizeof(*settings), 1, file);
+            fclose(file);
+        }
+        else
+            defaultSettings(settings);
+
+    }
+    else
+        defaultSettings(settings);
+
+    // free memory allocated by SHGetKnownFolderPath
+    CoTaskMemFree(path);
     return TRUE;
 }
 
 BOOL writeSettings(SETTINGS *settings)
 {
+    PWSTR path;
+    //const HRESULT hr = SHGetKnownFolderPath(&FOLDERID_RoamingAppData, 0, NULL, &path);
+
+    if (SUCCEEDED(SHGetKnownFolderPath(&FOLDERID_RoamingAppData, 0, NULL, &path)))
+    {
+        // create directory
+        PathAppendW(path, TEXT("DisplayLock"));
+        CreateDirectory(path, NULL);
+        // create file
+        PathAppendW(path, TEXT("\\settings.DLOCK"));
+        FILE *file = _wfopen(path, TEXT("w"));
+        if (file == NULL || file == 0)
+        {
+            CoTaskMemFree(path);
+            return FALSE;
+        }
+
+        fwrite(&settings, sizeof(settings), 1, file);
+        fclose(file);
+    }
+
+    // free memory allocated by SHGetKnownFolderPath
+    CoTaskMemFree(path);
     return TRUE;
 }
