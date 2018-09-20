11484cde225d94da14c37f0e28fe71b7a7afda7e
idietmoran <idietmoran@gmail.com>
2018-09-20 13:43:58 -0400

    added test for checkVersion

--- tests/unittest1.cpp
+++ tests/unittest1.cpp
@@ -17,14 +17,38 @@ namespace Tests
         {
             Logger::WriteMessage("defaultSettings()...");
             SETTINGS settings;
-            defaultSettings(&settings);
+            defaultSettings(&settings, L"5");
 
             // It's impossible to test the default version, as it's determined at run time
             Assert::AreEqual(settings.header, "DLOCK");
+            Assert::AreEqual(settings.version, 5);
             Assert::AreEqual(settings.borderless, FALSE);
             Assert::AreEqual(settings.foreground, FALSE);
             Assert::AreEqual(settings.fullScreen, FALSE);
             Assert::AreEqual(settings.minimize, TRUE);
+        }
+
+        TEST_METHOD(CheckVersion)
+        {
+            Logger::WriteMessage("CheckVersion()...");
+
+            SETTINGS settings;
+            defaultSettings(&settings, L"5");
+            Assert::AreEqual(checkVersion(&settings, L"5"), TRUE);
+            Assert::AreEqual(checkVersion(&settings, L"0005"), TRUE);
+            Assert::AreEqual(checkVersion(&settings, L"15"), FALSE);
+            Assert::AreEqual(checkVersion(&settings, L"A"), FALSE);
+            Assert::AreEqual(checkVersion(&settings, L"FOO"), FALSE);
+            Assert::AreEqual(checkVersion(&settings, L"F5"), FALSE);
+            
+            strcpy_s(settings.header, 4, "5A!");
+            Assert::AreEqual(checkVersion(&settings, L"5"), FALSE);
+            strcpy_s(settings.header, 4, "FOO");
+            Assert::AreEqual(checkVersion(&settings, L"5"), FALSE);
+            strcpy_s(settings.header, 4, "");
+            Assert::AreEqual(checkVersion(&settings, L"5"), FALSE);
+
+
         }
 
 		TEST_METHOD(ReadSettings)
