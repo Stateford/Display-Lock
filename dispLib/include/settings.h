d24f66f583e2a6399346b8b75ececf79e1a0ce27
idietmoran <idietmoran@gmail.com>
2018-09-21 22:55:12 -0400

    added path to file functions

--- dispLib/include/settings.h
+++ dispLib/include/settings.h
@@ -7,7 +7,8 @@ void initalizeSettings(HWND hDlg, SETTINGS_VIEW_CONTROLS *settingsControls);
 void setSettingsDlg(HWND hDlg, SETTINGS settings);
 void defaultSettings(SETTINGS *settings, wchar_t *versionStr);
 BOOL checkVersion(SETTINGS *settings, wchar_t *versionStr);
-BOOL writeSettings(SETTINGS settings);
-BOOL readSettings(SETTINGS *settings, wchar_t *versionStr);
 
 BOOL findPath(wchar_t *path);
+BOOL createDirectory(wchar_t *outPath);
+BOOL readSettings(SETTINGS *settings, wchar_t *versionStr, wchar_t *path);
+BOOL writeSettings(SETTINGS settings, wchar_t *path);
