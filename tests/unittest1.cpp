d193d499c49c60cc576dd6523c1e5e1ccca889bd
idietmoran <idietmoran@gmail.com>
2018-09-22 17:16:42 -0400

    added readFile Tests

--- tests/unittest1.cpp
+++ tests/unittest1.cpp
@@ -65,7 +65,21 @@ namespace Tests
 		TEST_METHOD(ReadSettings)
 		{
             Logger::WriteMessage("readSettings()...");
-			// TODO: Your test code here
+
+
+            SETTINGS settings = { 0 };
+            BOOL result;
+            result = readSettings(&settings, L"5", L"..\\..\\tests\\readTests\\test1.DLOCK");
+            Assert::AreEqual(result, TRUE, L"readSettings boolean, expected TRUE");
+
+            Assert::AreEqual(strcmp(settings.header, "DLOCK"), 0, L"settings header, expected DLOCK");
+            Assert::AreEqual(settings.version, 5, L"settings version, expected 5");
+            Assert::AreEqual(settings.minimize, FALSE, L"settings minimize, expected FALSE");
+            Assert::AreEqual(settings.foreground, FALSE, L"settings foreground, expected FALSE");
+            Assert::AreEqual(settings.borderless, FALSE, L"settings borderless, expected FALSE");
+            Assert::AreEqual(settings.fullScreen, FALSE, L"settings fullScreen, expected FALSE");
+
+
 		}
 
         TEST_METHOD(WriteSettings)
@@ -77,21 +91,22 @@ namespace Tests
 
             SETTINGS settings = {"DLOCK", 5, TRUE, TRUE, FALSE, FALSE};
 
-            result = writeSettings(settings, L"output.txt");
+            result = writeSettings(settings, L"output.DLOCK");
             Assert::AreEqual(result, TRUE, L"first test...");
 
             settings.version = 0;
 
-            result = writeSettings(settings, L"output.txt");
+            result = writeSettings(settings, L"output.DLOCK");
             Assert::AreEqual(result, FALSE, L"second test...");
 
             settings.version = 5;
 
             strcpy_s(settings.header, 6, "FOOBA");
 
-            result = writeSettings(settings, L".\\output.txt");
+            result = writeSettings(settings, L".\\output.DLOCK");
             Assert::AreEqual(result, FALSE, L"third test...");
 
+            DeleteFile(L"output.DLOCK");
             
         }
 
