9fd5a1dd875c44d5eb9ce40679723c75f6b309f6
idietmoran <idietmoran@gmail.com>
2018-09-23 11:02:19 -0400

    updated version settings

--- Display-Lock/resources/version.h
+++ Display-Lock/resources/version.h
@@ -1,5 +1,8 @@
 #pragma once
 
 #define CONFIGURATION ""
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 0
+#define VERSION_REVISION 0
 #define VERSION_BUILD 0
-#define FILE_VERSION "GENERATED.AUTOMATICALLY"
+#define FILE_VERSION ""
