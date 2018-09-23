009ee9d306ac7350f2cad3cbabcad6a775b2c590
idietmoran <idietmoran@gmail.com>
2018-09-22 22:00:19 -0400

    added system tray

---
+++ dispLib/src/notify.c
@@ -0,0 +1,31 @@
+#include "notify.h"
+
+
+void notifyInit(HWND hWnd, NOTIFYICONDATA * notify)
+{
+    notify->hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON));
+    notify->uVersion = 5;
+    notify->cbSize = sizeof(notify);
+    notify->hWnd = hWnd;
+    notify->uID = 1;
+    notify->uFlags = NIF_ICON | NIF_TIP | NIF_MESSAGE;
+    notify->uCallbackMessage = NOTIFY_MSG;
+
+}
+
+void showMainWindow(HWND hWnd, NOTIFYICONDATA *notify)
+{
+    Shell_NotifyIcon(NIM_DELETE, notify);
+    ShowWindow(hWnd, SW_RESTORE);
+    SetForegroundWindow(hWnd);
+}
+
+void showContext(HWND hWnd)
+{
+    POINT cursor;
+    GetCursorPos(&cursor);
+    HMENU menu = LoadMenu(NULL, MAKEINTRESOURCE(IDR_NOTIFY_MENU));
+    SetForegroundWindow(hWnd);
+    TrackPopupMenu(GetSubMenu(menu, 0), TPM_LEFTALIGN | TPM_LEFTBUTTON, cursor.x, cursor.y, 0, hWnd, 0);
+}
+
