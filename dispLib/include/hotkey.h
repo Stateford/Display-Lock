4fedf278cc327a1c9be5606c908f9d40adfcb924
idietmoran <idietmoran@gmail.com>
2018-09-29 10:18:13 -0400

    added base functions

--- dispLib/include/hotkey.h
+++ dispLib/include/hotkey.h
@@ -1,1 +1,7 @@
 #pragma once
+#include "common.h"
+
+void initHotkey(SETTINGS *settings);
+void createTable(SETTINGS *settings);
+void addHotkey(SETTINGS *settings);
+void removeHotkey(SETTINGS *settings);
