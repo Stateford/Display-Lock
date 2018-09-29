e3c9efc4c5561ea21b5a05cb61fa0b99e2cd451b
idietmoran <idietmoran@gmail.com>
2018-09-29 10:18:13 -0400

    added base functions

--- dispLib/src/hotkey.c
+++ dispLib/src/hotkey.c
@@ -1,1 +1,24 @@
 #include "hotkey.h"
+
+void initHotkey(SETTINGS *settings)
+{
+
+}
+
+void createTable(MAIN_WINDOW_CONTROLS *mainControls, SETTINGS *settings)
+{
+    mainControls->userHotkeys = CreateAcceleratorTable(settings->hotkeys, settings->hotkeySize);
+}
+
+void addHotkey(SETTINGS *settings)
+{
+
+}
+
+void removeHotkey(MAIN_WINDOW_CONTROLS *mainControls, SETTINGS *settings)
+{
+    // dest
+    DestroyAcceleratorTable(mainControls->userHotkeys);
+    
+
+}
