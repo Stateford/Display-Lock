849528ce69811ec58297f188cd0e5d0496a6afca
Dimitri <idietmoran@gmail.com>
2018-10-06 09:58:53 -0400

    Codefactor fixes (#78)
    * fixed potential conflicts
    
    * updated script for new config version

--- scripts/buildInc.py
+++ scripts/buildInc.py
@@ -7,20 +7,19 @@ automatically increments build number and configuration
 import sys
 import os
 
-
 def writeHeader(filePath, config, version, build):
     print("Writing to path: %s\n" % filePath)
     versSplit = version.split('.')
     
-    file = open(filePath, 'w+')
-    file.write("#pragma once\n\n")
-    file.write("#define CONFIGURATION \"%s\"\n" % config)
-    file.write("#define VERSION_MAJOR %d\n" % int(versSplit[0]))
-    file.write("#define VERSION_MINOR %d\n" % int(versSplit[1]))
-    file.write("#define VERSION_REVISION %d\n" % int(versSplit[2]))
-    file.write("#define VERSION_BUILD %s\n" % build)
-    file.write("#define FILE_VERSION \"%s.%s\"\n" % (version, build))
-    file.close()
+    versionFile = open(filePath, 'w+')
+    versionFile.write("#pragma once\n\n")
+    versionFile.write("#define CONFIGURATION \"%s\"\n" % config)
+    versionFile.write("#define VERSION_MAJOR %d\n" % int(versSplit[0]))
+    versionFile.write("#define VERSION_MINOR %d\n" % int(versSplit[1]))
+    versionFile.write("#define VERSION_REVISION %d\n" % int(versSplit[2]))
+    versionFile.write("#define VERSION_BUILD %s\n" % build)
+    versionFile.write("#define FILE_VERSION \"%s.%s\"\n" % (version, build))
+    versionFile.close()
 
 def main():
     print(sys.argv)
@@ -28,6 +27,5 @@ def main():
         raise BaseException("Something went wrong with build")
     
     writeHeader(os.path.abspath("./Display-Lock/resources/version.h"), sys.argv[1], sys.argv[2], sys.argv[3])
-
 
 main()
