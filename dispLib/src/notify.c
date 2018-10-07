064a4d2350687d0bb07503c0d79b30035359e5aa
idietmoran <idietmoran@gmail.com>
2018-10-06 22:04:48 -0400

    Added system tray windows context menu

--- dispLib/src/notify.c
+++ dispLib/src/notify.c
@@ -1,4 +1,5 @@
 #include "notify.h"
+#include "win.h"
 
 
 void notifyInit(HWND hWnd, NOTIFYICONDATA * notify)
@@ -21,16 +22,36 @@ void showMainWindow(HWND hWnd, NOTIFYICONDATA *notify)
 
 void showContext(HWND hWnd, HMENU menu)
 {
+    HMENU contextMenu = GetSubMenu(menu, 0);
+    // create new windowmenu
+    HMENU windowMenu = CreatePopupMenu();
+    // populate window list
+    updateContextMenu(&windowMenu);
+    // modify the popup with the new window list
+    ModifyMenu(contextMenu, 3, MF_BYPOSITION | MF_POPUP, (UINT)windowMenu, TEXT("Windows"));
+
     POINT cursor;
     GetCursorPos(&cursor);
-    
     SetForegroundWindow(hWnd);
-    TrackPopupMenu(GetSubMenu(menu, 0), TPM_LEFTALIGN | TPM_LEFTBUTTON, cursor.x, cursor.y, 0, hWnd, 0);
+    // open the popup menu
+    TrackPopupMenu(contextMenu, TPM_LEFTALIGN | TPM_LEFTBUTTON, cursor.x, cursor.y, 0, hWnd, 0);
 }
 
 void notifyChildWindows(HWND hWnd, UINT msg)
 {
     EnumChildWindows(hWnd, EnumChildProc, (LPARAM)msg);
+}
+
+void updateContextMenu(HMENU *submenu)
+{
+    WINDOWLIST windows;
+    openWindows(&windows);
+
+    for (int i = 0; i < windows.count; i++)
+    {
+        AppendMenuA(*submenu, MF_STRING, (100 + i), windows.windows[i].title);
+    }
+
 }
 
 BOOL EnumChildProc(HWND hWnd, LPARAM lParam)
