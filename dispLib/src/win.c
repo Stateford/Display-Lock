3ff19f145de2d6783b385fedbe6e3159ef1ee2c9
Rasmus Michelsen <RMichelsen@protonmail.com>
2018-10-08 17:10:59 +0200

    Removed redundant if statement.

--- dispLib/src/win.c
+++ dispLib/src/win.c
@@ -50,11 +50,7 @@ BOOL CALLBACK EnumChildProcUWP(HWND hwnd, LPARAM lParam)
 	GetClassNameA(hwnd, className, 500);
 
 	// Checks if the child window is a CoreWindow (this seems to signify whether there is a window open)
-	if (!strcmp(className, "Windows.UI.Core.CoreWindow")) 
-	{
-		return FALSE;
-	}
-	return TRUE;
+	return !(!strcmp(className, "Windows.UI.Core.CoreWindow")); 
 }
 
 // enumerate windows and get current window list
