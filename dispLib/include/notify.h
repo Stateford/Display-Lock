071a9e134ba4ffb9e2599662ebdd82fca5094b83
idietmoran <idietmoran@gmail.com>
2018-10-08 21:50:50 -0400

    fixed context menu options being enabled

--- dispLib/include/notify.h
+++ dispLib/include/notify.h
@@ -6,7 +6,7 @@
 
 void notifyInit(HWND hWnd, NOTIFYICONDATA *notify);
 void showMainWindow(HWND hWnd, NOTIFYICONDATA *notify);
-void showContext(HWND hWnd, HMENU menu, WINDOWLIST *windows, SETTINGS settings);
+void showContext(HWND hWnd, WINDOWLIST *windows, SETTINGS settings, BOOL running);
 void notifyChildWindows(HWND hWnd, UINT msg);
 void drawMenuSettings(HMENU menu, SETTINGS settings);
 void updateContextMenu(HMENU *submenu, WINDOWLIST *windows);
