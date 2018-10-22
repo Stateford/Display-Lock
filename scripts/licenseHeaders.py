dab8f097b74d7bf404f7462d4fd34908399e302d
idietmoran <idietmoran@gmail.com>
2018-10-21 20:20:14 -0400

    added script to attach license headers

--- scripts/licenseHeaders.py
+++ scripts/licenseHeaders.py
@@ -1,28 +1,42 @@
+"""
+
+"""
 import os
 
 class FileWriter:
 
+
     def __init__(self, path):
         self.path = path
-
-    def __enter__(self):
-        self.file = open(self.path, "w")
-
-    def __exit__(self, a, b, c):
+        self.file = open(self.path, "r")
+        self.contents = self.file.readlines()
         self.file.close()
+        
 
     def write(self):
-        self.file.write("test")
+        """
+        write
+        inserts license as a header to the file
+        """
+        # header
+        self.file = open(self.path, "w")    
+        self.file.write("test\n")
+        self.file.writelines(self.contents)
+        self.file.close()
 
 class License:
-    extensions = ['.h', '.c', '.cpp']
+    extensions = ['.h', '.c', '.cpp', '.py']
+    excludedFiles = ['versionRC.h', 'resource.h', 'targetver.h', 'header.h', 'common.h']
+
 
     def __init__(self):
         self.cwd = os.getcwd()
 
+
     def __enter__(self):
         os.chdir("..")
         self.parentDir = os.listdir()
+
 
     def __exit__(self, a, b, c):
         os.chdir(self.cwd)
@@ -30,7 +44,6 @@ class License:
     def parse(self, path, directory):
         for it in directory:
             newPath = "%s\\%s" % (path, it)
-            #print(newPath)
 
             if(os.path.isdir(newPath)):
                 dirs = os.listdir(newPath)
@@ -40,7 +53,11 @@ class License:
             elif(os.path.isfile(newPath)):
                 _, file_extension = os.path.splitext(newPath)
                 if file_extension in self.extensions:
-                    print(it)
+                    filewriter = FileWriter(newPath)
+                    filewriter.write()
+
+    def _check_excluded(self):
+        pass
 
     def start(self):
         self.parse(os.getcwd(), self.parentDir)
