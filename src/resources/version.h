a4485f4f4c3a4220ce55361802a2302869ec65fd
Dimitri Apostal <idietmoran@gmail.com>
2023-02-11 23:21:58 -0500

    added updating versioning through cmake

--- src/resources/version.h
+++ src/resources/version.h
@@ -1,8 +1,25 @@
 #pragma once
 
+#ifndef CONFIGURATION
 #define CONFIGURATION ""
+#endif
+
+#ifndef VERSION_MAJOR
 #define VERSION_MAJOR 0
+#endif
+
+#ifndef VERSION_MINOR
 #define VERSION_MINOR 0
+#endif
+
+#ifndef VERSION_REVISION
 #define VERSION_REVISION 0
+#endif
+
+#ifndef VERSION_BUILD
 #define VERSION_BUILD 0
+#endif
+
+#ifndef FILE_VERSION
 #define FILE_VERSION ""
+#endif
